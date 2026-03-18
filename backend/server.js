ea4ec38d66ee454a2ae35a5b2794b00d65ddcc7c
JonathanLuis <jdk99.rodrigues@gmail.com>
2026-03-17 22:15:08 -0300

    inseri PORT para deploy no render

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ app.use(express.json())
 
 app.use("/api", taskRoutes)
 
-app.listen(5000, () => {
-  console.log("Servidor rodando na porta 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando")
 })
